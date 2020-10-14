16746a7f9e2c7bb1181f6b0571fd2c28b08dd5a1
Shivayan Bora 1022042 <shivayananand.bora@blueyonder.com>
2020-10-14 12:32:00 +0530

    Finished Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NewBookForm from './components/BookForm';
+import BookList from './components/BookList';
+import NavBar from './components/NavBar';
+import BookContextProvider from './contexts/BookContext';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BookContextProvider>
+        <NavBar/>
+        <BookList/>
+        <NewBookForm/>
+      </BookContextProvider>
     </div>
   );
 }
